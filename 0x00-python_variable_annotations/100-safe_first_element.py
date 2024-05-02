fd9c3139e8e8be1ea8d995df55ec150b4f7d157b
Hatim Selmun <selmun13@gmail.com>
2024-05-02 16:11:05 +0100

    duck tying- first element of a sequence

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Duck typing- first element of a sequence"""
+from typing import Sequence, Any, Union, NoneType
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
+    if lst:
+        return lst[0]
+    else:
+        return None
