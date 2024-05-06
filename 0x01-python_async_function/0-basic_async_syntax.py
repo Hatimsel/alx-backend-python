92ec69337d3f8784bd2cd3e83280f9da9a0e38e4
Hatim Selmun <selmun13@gmail.com>
2024-05-06 11:11:23 +0100

    basic syntax of async/await

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int =10) -> float :
+async def wait_random(max_delay: int = 10) -> float:
     """Takes an int
     waits a random delay between 0, and the int passed
     Returns the time waited"""
