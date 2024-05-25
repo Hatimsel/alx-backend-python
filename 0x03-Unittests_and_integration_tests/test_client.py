4f12f0dab49fea61fc940a39530f5fab7338649c
Hatim Selmun <selmun13@gmail.com>
2024-05-25 23:37:13 +0100

    integration test

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """Testing client.GithubOrgClient class"""
+import requests
 from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
 from parameterized import parameterized
 import mock
 import unittest
-from unittest.mock import patch
-from parameterized import parameterized
+from unittest.mock import patch, MagicMock
+from parameterized import parameterized, parameterized_class
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -63,3 +65,75 @@ class TestGithubOrgClient(unittest.TestCase):
         """Testing GithubOrgClient.has_license"""
         self.assertEqual(GithubOrgClient.has_license(mapping, value),
                          expected_result)
+
+
+@parameterized_class(('org_payload', 'repos_payload',
+                      'expected_repos', 'apache2_repos'), [
+                          (TEST_PAYLOAD[0][0], TEST_PAYLOAD[0][1],
+                           TEST_PAYLOAD[0][2], TEST_PAYLOAD[0][3])
+                      ])
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """Testing GithubOrgClient.public_repos"""
+    @classmethod
+    def setUpClass(cls):
+        cls.get_patcher = patch('utils.requests.get')
+        mocked_get = cls.get_patcher.start()
+
+        # def side_effect(url):
+        #     if url == 'url_for_org_payload':
+        #         return cls.org_payload
+        #     elif url == 'url_for_repos_payload':
+        #         return cls.repos_payload
+        #     elif url == 'url_for_expected_repos':
+        #         return cls.expected_repos
+        #     elif url == 'url_for_apache2_repos':
+        #         return cls.apache2_repos
+        #     else:
+        #         raise ValueError(f'Unexpected URL: {url}')
+
+        # mocked_get.side_effect = side_effect
+        response_mock = MagicMock()
+
+        response_mock.json.side_effect = [
+            (cls.org_payload, cls.repos_payload,
+             cls.expected_payload, cls.apache2_repos)
+             ]
+
+        mocked_get.return_value = response_mock
+
+        instance = GithubOrgClient('google')
+        result = instance.public_repos()
+
+        cls.assertEqual(result, mocked_get.return_value)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.get_patcher.stop()
+
+    # def test_public_repos_with_org_payload(self):
+    #     """Testing public_repos with org payload"""
+    #     instance = GithubOrgClient('google')
+    #     result = instance.public_repos()
+
+    #     self.assertEqual(result, self.org_payload)
+
+    # def test_public_repos_with_repos_payload(self):
+    #     """Testing public_repos with repos payload"""
+    #     instance = GithubOrgClient('google')
+    #     result = instance.public_repos()
+
+    #     self.assertEqual(result, self.repos_payload)
+
+    # def test_public_repos_with_expected_repos(self):
+    #     """Testing public_repos with expected_repos"""
+    #     instance = GithubOrgClient('google')
+    #     result = instance.public_repos()
+
+    #     self.assertEqual(result, self.expected_repos)
+
+    # def test_public_repos_with_apache2_repos(self):
+    #     """Testing public_repos with apache2_repos"""
+    #     instance = GithubOrgClient('google')
+    #     result = instance.public_repos()
+
+    #     self.assertEqual(result, self.apache2_repos)
